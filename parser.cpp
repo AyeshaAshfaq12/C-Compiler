f513ff3881e540f849ed2abc7cbc7617da101bd9
AyeshaAshfaq12 <ayeshaashfaq925@gmail.com>
2024-11-11 18:14:37 +0500

    Symbol Table implement

--- parser.cpp
+++ parser.cpp
@@ -3,6 +3,7 @@
 #include<string>
 #include<cctype>
 #include<map>
+#include <unordered_map>
 
 using namespace std;
 
@@ -48,6 +49,27 @@ class Lexer{
             return src.substr(start, pos - start);
         }
 
+            string consumeString() {
+                size_t start = pos + 1;
+                pos++;
+
+                while (pos < src.size() && src[pos] != '"') {
+                    if (src[pos] == '\n') {
+                        line++;
+                    }
+                    pos++;
+                }
+                
+                if (pos < src.size() && src[pos] == '"') {
+                    string strValue = src.substr(start, pos - start);
+                    pos++;
+                    return strValue;
+                }
+                
+                cout << "Error: Unmatched quotation mark at line " << line << endl;
+                exit(1);
+            }
+
         vector<Token> tokenize(){
             vector<Token> tokens;
             while(pos < src.size()){
@@ -57,53 +79,78 @@ class Lexer{
                     pos++;
                     continue;
                 }
-                if(isdigit(current)){
-                    tokens.push_back(Token{T_NUM , consumeNumber()});
+                
+                if (current == '/' && pos + 1 < src.size() && src[pos + 1] == '/') {
+                    while (pos < src.size() && src[pos] != '\n') {
+                        pos++;
+                    }
                     continue;
                 }
-                if(isalpha(current)){
+
+                if (current == '"') {
+                    string str = consumeString();
+                    tokens.push_back(Token{T_STRING, str, line});
+                    continue;
+                }
+
+                if (isdigit(current)) {
+                    string number = consumeNumber();
+                    if (pos < src.size() && src[pos] == '.') {
+                        pos++;
+                        number += "." + consumeNumber();
+                        tokens.push_back(Token{T_FLOAT, number, line});
+                    } else {
+                        tokens.push_back(Token{T_NUM, number, line});
+                    }
+                    continue;
+                }
+
+                if (isalpha(current) || current == '_') {
                     string word = consumeWord();
-                    if (word == "int") tokens.push_back(Token{T_INT , word, line});
-                    else if (word == "float") tokens.push_back(Token{T_FLOAT , word, line});
-                    else if (word == "double") tokens.push_back(Token{T_DOUBLE , word, line});
-                    else if (word == "bool") tokens.push_back(Token{T_BOOL , word, line});
-                    else if (word == "char") tokens.push_back(Token{T_CHAR , word, line});
-                    else if (word == "string") tokens.push_back(Token{T_STRING , word, line});
-                    else if (word == "IF") tokens.push_back(Token{T_IF , word, line});
-                    else if (word == "else") tokens.push_back(Token{T_ELSE , word, line});
-                    else if (word == "return") tokens.push_back(Token{T_RETURN , word, line});
-                    else if (word == "while") tokens.push_back(Token{T_WHILE , word, line});
-                    else tokens.push_back(Token{T_ID , word, line});
+                    if (word == "int") tokens.push_back(Token{T_INT, word, line});
+                    else if (word == "float") tokens.push_back(Token{T_FLOAT, word, line});
+                    else if (word == "double") tokens.push_back(Token{T_DOUBLE, word, line});
+                    else if (word == "bool") tokens.push_back(Token{T_BOOL, word, line});
+                    else if (word == "char") tokens.push_back(Token{T_CHAR, word, line});
+                    else if (word == "string") tokens.push_back(Token{T_STRING, word, line});
+                    else if (word == "if") tokens.push_back(Token{T_IF, word, line});
+                    else if (word == "else") tokens.push_back(Token{T_ELSE, word, line});
+                    else if (word == "return") tokens.push_back(Token{T_RETURN, word, line});
+                    else if (word == "while") tokens.push_back(Token{T_WHILE, word, line});
+                    else if (word == "for") tokens.push_back(Token{T_FOR, word, line});
+                    else tokens.push_back(Token{T_ID, word, line});
                     continue;
                 }
 
                 switch(current){
-                    case '=' : 
-                        if (src[pos+1] == '=') { tokens.push_back(Token{T_EQ, "==", line}); pos++; }
-                        else { tokens.push_back(Token{T_ASSIGN , "=", line}); } 
+                    case '=':
+                        if (src[pos + 1] == '=') { tokens.push_back(Token{T_EQ, "==", line}); pos++; }
+                        else { tokens.push_back(Token{T_ASSIGN, "=", line}); }
                         break;
-                    case '!' :
-                        if (src[pos+1] == '=') { tokens.push_back(Token{T_NEQ, "!=", line}); pos++; }
+                    case '!':
+                        if (src[pos + 1] == '=') { tokens.push_back(Token{T_NEQ, "!=", line}); pos++; }
                         break;
-                    case '&' :
-                        if (src[pos+1] == '&') { tokens.push_back(Token{T_LOGICAL_AND, "&&", line}); pos++; }
+                    case '&':
+                        if (src[pos + 1] == '&') { tokens.push_back(Token{T_LOGICAL_AND, "&&", line}); pos++; }
                         break;
-                    case '|' :
-                        if (src[pos+1] == '|') { tokens.push_back(Token{T_LOGICAL_OR, "||", line}); pos++; }
+                    case '|':
+                        if (src[pos + 1] == '|') { tokens.push_back(Token{T_LOGICAL_OR, "||", line}); pos++; }
                         break;
-                    case '+' : tokens.push_back(Token{T_PLUS , "+", line}); break;
-                    case '-' : tokens.push_back(Token{T_MINUS , "-", line}); break;
-                    case '*' : tokens.push_back(Token{T_MUL , "*", line}); break;
-                    case '/' : tokens.push_back(Token{T_DIV , "/", line}); break;
-                    case '(' : tokens.push_back(Token{T_LPAREN , "(", line}); break;
-                    case ')' : tokens.push_back(Token{T_RPAREN , ")", line}); break;
-                    case '{' : tokens.push_back(Token{T_LBRACE , "{", line}); break;
-                    case '}' : tokens.push_back(Token{T_RBRACE , "}", line}); break;
-                    case ';' : tokens.push_back(Token{T_SEMICOLON , ";", line}); break;
-                    case ',' : tokens.push_back(Token{T_COMMA , ",", line}); break;
-                    case '<' : tokens.push_back(Token{T_LT , "<", line}); break;
-                    case '>' : tokens.push_back(Token{T_GT , ">", line}); break;
-                    default: cout << "Unexpected character: " << current << endl; exit(1);
+                    case '+': tokens.push_back(Token{T_PLUS, "+", line}); break;
+                    case '-': tokens.push_back(Token{T_MINUS, "-", line}); break;
+                    case '*': tokens.push_back(Token{T_MUL, "*", line}); break;
+                    case '/': tokens.push_back(Token{T_DIV, "/", line}); break;
+                    case '(': tokens.push_back(Token{T_LPAREN, "(", line}); break;
+                    case ')': tokens.push_back(Token{T_RPAREN, ")", line}); break;
+                    case '{': tokens.push_back(Token{T_LBRACE, "{", line}); break;
+                    case '}': tokens.push_back(Token{T_RBRACE, "}", line}); break;
+                    case ';': tokens.push_back(Token{T_SEMICOLON, ";", line}); break;
+                    case ',': tokens.push_back(Token{T_COMMA, ",", line}); break;
+                    case '<': tokens.push_back(Token{T_LT, "<", line}); break;
+                    case '>': tokens.push_back(Token{T_GT, ">", line}); break;
+                    default:
+                        cout << "Unexpected character: '" << current << "' at line " << line << endl;
+                        exit(1);
                 }
                 pos++;
             }
@@ -146,8 +193,15 @@ class Parser{
             expect(T_RBRACE);
         }
 
-        void parseDeclaration(){
-            expect(T_INT);
+        void parseDeclaration() {
+            if (tokens[pos].type == T_INT || tokens[pos].type == T_FLOAT || tokens[pos].type == T_DOUBLE ||
+                tokens[pos].type == T_BOOL || tokens[pos].type == T_CHAR || tokens[pos].type == T_STRING) {
+                pos++;
+            } else {
+                cout << "Syntax error on line " << tokens[pos].line << ": Expected a data type but found " 
+                    << tokens[pos].value << endl;
+                exit(1);
+            }          
             expect(T_ID);
             expect(T_SEMICOLON);
         }
@@ -243,11 +297,49 @@ class Parser{
     
 };
 
+enum SymbolType {
+    INT, FLOAT, STRING, BOOL, CHAR, DOUBLE
+};
+
+struct Symbol {
+    SymbolType type;
+    string name;
+    int line;
+    bool isInitialized;
+};
+
+class SymbolTable {
+    private:
+        unordered_map<string, Symbol> table;
+
+    public:
+        void insert(const string &name, SymbolType type, int line) {
+            if (table.find(name) != table.end()) {
+                cout << "Error: Symbol " << name << " already declared at line " << table[name].line << endl;
+            } else {
+                table[name] = Symbol{type, name, line, false};
+            }
+        }
+
+        Symbol* lookup(const string &name) {
+            if (table.find(name) != table.end()) {
+                return &table[name];
+            } else {
+                cout << "Error: Symbol " << name << " not found" << endl;
+                return nullptr;
+            }
+        }
+
+        
+};
+
+
 int main(){
     string input = R"(
         int a;
         a = 5;
         int b;
+        string name = "Ayesha";
         b = a + 10;
         if(b > 10){
             return b;
